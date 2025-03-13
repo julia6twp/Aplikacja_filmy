0d73ed29b0bdfaffc0b2f43f0a54200337d910f2
julia6twp <julia.kluk6@wp.pl>
2025-03-14 00:01:39 +0100

    Pobieranie API o filmach

---
+++ demo/src/main/java/com/example/demo/controller/JustWatchController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.JustWatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/justwatch")
+public class JustWatchController {
+
+    private final JustWatchService justWatchService;
+
+    @Autowired
+    public JustWatchController(JustWatchService justWatchService) {
+        this.justWatchService = justWatchService;
+    }
+
+
+    @GetMapping("/film/{filmName}")
+    public String getFilm(@PathVariable String filmName) {
+        return justWatchService.getFilmInfo(filmName);
+    }
+
+
+    @GetMapping("/searchTerm/{searchTerm}")
+    public String searchFilmBySearchTerm(@PathVariable String searchTerm) {
+        return justWatchService.searchFilmBySearchTerm(searchTerm);
+    }
+
+    @GetMapping("/searchTopRanked")
+    public String searchTopRankedFilms(){
+        return justWatchService.searchTopRankedFilms();
+    }
+
+    @GetMapping("/searchPopularFilms")
+    public String searchPopularFilms(){
+        return justWatchService.searchPopularFilms();
+    }
+}
---
+++ demo/src/main/java/com/example/demo/service/JustWatchService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class JustWatchService {
+    private final String apiUrl="https://api.themoviedb.org/3/movie/";
+
+    @Value("${justwatch.api.key}")
+    private String apiKey;
+
+    //nie używać
+    public String getFilmInfo(String title) {
+        String url=apiUrl+title+"/watch/providers?api_key="+apiKey;
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class);
+    }
+
+
+    public String searchFilmBySearchTerm(String searchTerm) {
+        String url="https://api.themoviedb.org/3/search/movie?query="+searchTerm+"&api_key="+apiKey;
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class);
+    }
+
+
+    public String searchTopRankedFilms(){
+        String url="https://api.themoviedb.org/3/movie/top_rated?api_key="+apiKey+"&language=en-US&page=1";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class);
+    }
+
+
+    public String searchPopularFilms(){
+        String url="https://api.themoviedb.org/3/movie/popular?api_key="+apiKey+"&language=en-US&page=1";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class);
+    }
+
+}
